bb8d97e922d9f05e6e81540a397ba278d62d3718
Nijamudeen Mohamed Sulaiman <nijamudeensulaiman@syncfusion.com>
2019-12-19 15:11:35 +0530

    Flutter calendar getting started sample added

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calendar Getting Started',
+      home: Scaffold(
+        body: SfCalendar(
+          view: CalendarView.month,
+          dataSource: MeetingDataSource(_getDataSource()),
+          monthViewSettings: MonthViewSettings(
+              appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+        ),
+      ),
+    );
+  }
+
+  List<Meeting> _getDataSource() {
+    List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime =
+        DateTime(today.year, today.month, today.day, 9, 0, 0);
+    final DateTime endTime = startTime.add(Duration(hours: 2));
+    meetings.add(
+        Meeting("Conference", startTime, endTime, Color(0xFF0F8644), false));
+    return meetings;
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(this.source);
+
+  List<Meeting> source;
+
+  @override
+  List get appointments => source;
+
+  @override
+  DateTime getStartTime(int index) {
+    return source[index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return source[index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return source[index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return source[index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return source[index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+}
